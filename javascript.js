1c88d02feb3cf5c551c7bc0f6c9362b3dcd5619c
Tornikekvirik <tornike.kvirikashvili22b@geolab.edu.ge>
2021-11-03 04:29:17 +0400

    .

---
+++ javascript.js
@@ -0,0 +1,55 @@
+
+     // ცვლადები  
+// const firstName = 'Tornike'
+// const isprogrammer = true
+// const age = 21
+
+// console.log(isprogrammer)
+// console.log(name)
+// console.log('Name: ' + firstName + ',  age: ' + age)
+// const lastname = prompt('Lastname')
+// alert(firstName + ' ' + lastname)
+    //ოპერატორი
+// let currentYear = 2021
+// const birthYear = 2000
+// console.log(currentYear)
+
+// const a = 10
+// const b = 20
+// let c = 32
+// c = c + a
+// console.log(c)
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+
+// const isprogrammer = true
+// const Name = 'Tornike'
+// const age = 21
+// let x
+// console.log(typeof isprogrammer)
+// console.log(typeof Name)
+// console.log(typeof age)
+// console.log(typeof x)
+
+
+// const fullAge = 21
+// const birthYear = 2000
+// const currentYear = 2021                                 ERROR
+
+// const isfullage = currentYear - birthYear <= fullAge
+// consolle.log(isfullage)
+
+
+
+// const Status = 'ready'
+
+// if (Status === 'ready') {
+//     console/log('მზადარის')
+// }
+//                                                     // რატო მიერორებს რედის და პენდინგს სწორად ამუშავებს ბლიად
+// if (Status === 'pending') {
+//     console.log('მუშავდება')
+// }
